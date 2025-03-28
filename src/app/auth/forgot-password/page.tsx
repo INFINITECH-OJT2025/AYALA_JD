74cb88ca8de5dc204766458fba420a3a112c0ef8
anyayahan <infinitechojt2025@gmail.com>
2025-03-28 16:44:18 +0800

    params padin

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -1,12 +1,7 @@
-export const dynamic = "force-dynamic";
+// src/app/(auth)/forgot-password/page.tsx
 
-import { useSearchParams } from "next/navigation";
-import { ForgotPassword } from "@/components/auth/forgot-password";
+import ForgotPasswordPageContent from "./forgot-password-content";
 
-export default function ForgotPasswordPageContent() {
-    const searchParams = useSearchParams();
-    const email = searchParams.get("email") || "";
-    const token = searchParams.get("token") || "";
-
-    return <ForgotPassword email={email} token={token} />;
+export default function ForgotPasswordPage() {
+    return <ForgotPasswordPageContent />;
 }
