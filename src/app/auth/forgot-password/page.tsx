7b1117ca2a4ca25a40356086ade87559fcdcaf82
anyayahan <infinitechojt2025@gmail.com>
2025-03-28 15:52:10 +0800

    Update page.tsx

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -1,7 +1,11 @@
-"use client"
-
+// app/auth/forgot-password/page.tsx
 import { ForgotPassword } from "@/components/auth/forgot-password";
+import { Suspense } from "react";
 
-export default function ForgotPasswordPage() {
-  return <ForgotPassword />;
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ForgotPassword/>
+    </Suspense>
+  );
 }
