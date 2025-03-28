029e42f64f9c175191d9348efa2b3103ba23d4b7
anyayahan <infinitechojt2025@gmail.com>
2025-03-28 16:15:22 +0800

    Update page.tsx

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -1,9 +1,13 @@
+// app/(auth)/forgot-password/forgot-password-content.tsx
+"use client";
+
+import { useSearchParams } from "next/navigation";
 import { ForgotPassword } from "@/components/auth/forgot-password";
 
-
-export default function ForgotPasswordPage({ searchParams }: { searchParams: { email?: string; token?: string } }) {
-    const email = searchParams.email || "";
-    const token = searchParams.token || "";
+export default function ForgotPasswordPageContent() {
+    const searchParams = useSearchParams();
+    const email = searchParams.get("email") || "";
+    const token = searchParams.get("token") || "";
 
     return <ForgotPassword email={email} token={token} />;
 }
